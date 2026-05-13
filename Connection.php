d1ca3de4a283bf099217d37b332ab0b7c0e3bb15
reshadrahim <reshadrahim560@gmail.com>
2026-05-14 02:51:54 +0600

    Create Connection.php

---
+++ Connection.php
@@ -0,0 +1,21 @@
+<?php
+
+error_reporting(0);
+
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "employee";
+
+$conn =mysqli_connect($servername,$username,$password,$dbname);
+
+if($conn)
+{
+	//echo "Connection OK";
+}
+else
+{
+	echo "Connection FAILD".mysqli_connect_error();
+}
+
+?>
